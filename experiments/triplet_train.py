2caef6c2d1369c94fe15819d09cc4c718739f9b8
BratchikovD <bratchikov.dmitry@gmail.com>
2023-05-14 02:39:04 +0300

    ADD test ADD results

--- experiments/triplet_train.py
+++ experiments/triplet_train.py
@@ -1,4 +1,5 @@
 import json
+from multiprocessing import freeze_support
 from pathlib import Path
 import os
 import torch
@@ -10,93 +11,97 @@ from helpers.utils import log_to_file, get_accuracy
 from pytorch_metric_learning import miners, distances, losses
 from tqdm import tqdm
 
-BASE_DIR = Path(__file__).parent.parent
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-TRAIN_DIR = os.path.join(DATA_DIR, 'train')
-TEST_DIR = os.path.join(DATA_DIR, 'test')
-VAL_DIR = os.path.join(DATA_DIR, 'val')
 
-h, w = 224, 224
+if __name__ == '__main__':
+    freeze_support()
 
-transform_train_list = [
-    transforms.Resize((h, w), interpolation=3),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-]
+    BASE_DIR = Path(__file__).parent.parent
+    DATA_DIR = os.path.join(BASE_DIR, 'data')
+    TRAIN_DIR = os.path.join(DATA_DIR, 'train')
+    VAL_DIR = os.path.join(DATA_DIR, 'val')
 
-data_transforms = {
-    'train': transforms.Compose(transform_train_list),
-}
+    h, w = 224, 224
 
-train_dataset = ImageFolder(TRAIN_DIR, data_transforms['train'])
-val_dataset = ImageFolder(VAL_DIR, data_transforms['train'])
+    transform_train_list = [
+        transforms.Resize((h, w), interpolation=3),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]
 
-dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=64,
-                                         shuffle=True, num_workers=2, pin_memory=True,
-                                         prefetch_factor=2, persistent_workers=True)
+    data_transforms = {
+        'train': transforms.Compose(transform_train_list),
+    }
 
-dataset_size = len(train_dataset)
-class_names = train_dataset.classes
+    train_dataset = ImageFolder(TRAIN_DIR, data_transforms['train'])
+    val_dataset = ImageFolder(VAL_DIR, data_transforms['train'])
 
-DEVICE = torch.device("cuda:0")
+    BATCH_SIZE = 16
+    dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE,
+                                             shuffle=True, num_workers=2, pin_memory=True,
+                                             prefetch_factor=2, persistent_workers=True)
 
-model = models.resnet50(pretrained=True).cuda()
-distance = distances.CosineSimilarity()
-criterion = losses.TripletMarginLoss(margin=0.2, distance=distance)
-mining_func = miners.TripletMarginMiner(margin=0.2, distance=distance, type_of_triplets="hard")
-optimizer = optim.SGD([
-    {'params': model.parameters(), 'lr': 0.001},
-], weight_decay=5e-4, momentum=0.9, nesterov=True)
+    dataset_size = len(train_dataset)
+    class_names = train_dataset.classes
 
-EPOCHS = 25
-SAVE_PATH = '../results/TripletLoss'
-history = {"train": [], "val": [], "best_accuracy": 0.0}
+    DEVICE = torch.device("cuda:0")
+    TRIPLETS_TYPE = "semihard"
+    model = models.resnet50(pretrained=True).cuda()
+    distance = distances.CosineSimilarity()
+    criterion = losses.TripletMarginLoss(margin=0.2, distance=distance)
+    mining_func = miners.TripletMarginMiner(margin=0.2, distance=distance, type_of_triplets=TRIPLETS_TYPE)
+    optimizer = optim.SGD([
+        {'params': model.parameters(), 'lr': 0.001},
+    ], weight_decay=5e-4, momentum=0.9, nesterov=True)
 
-os.makedirs(SAVE_PATH, exist_ok=True)
-if os.path.exists("training.log"):
-    os.remove("training.log")
+    EPOCHS = 25
+    SAVE_PATH = f'../results/TripletLoss_{EPOCHS}_{BATCH_SIZE}_{TRIPLETS_TYPE}'
+    history = {"train": [], "val": [], "best_accuracy": 0.0}
 
-for epoch in range(EPOCHS):
-    model.to(DEVICE)
-    model.train()
+    os.makedirs(SAVE_PATH, exist_ok=True)
+    if os.path.exists(f"{SAVE_PATH}/training.log"):
+        os.remove("training.log")
 
-    for index, (inputs, labels) in (enumerate(tqdm(dataloader, desc="Training", leave=False))):
-        inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
+    for epoch in range(EPOCHS):
+        model.to(DEVICE)
+        model.train()
 
-        optimizer.zero_grad()
-        embeddings = model(inputs)
+        for index, (inputs, labels) in (enumerate(tqdm(dataloader, desc="Training", leave=False))):
+            inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
 
-        triplets = mining_func(embeddings, labels)
+            optimizer.zero_grad()
+            embeddings = model(inputs)
 
-        loss = criterion(embeddings, labels, triplets)
-        loss.backward()
+            triplets = mining_func(embeddings, labels)
 
-        optimizer.step()
+            loss = criterion(embeddings, labels, triplets)
+            loss.backward()
 
-        if index % 100 == 0:
-            history["train"].append({
-                "epoch": epoch,
-                "loss": loss.item(),
-                "triplets": mining_func.num_triplets
-            })
-            msg = f"Эпоха [{epoch}/{EPOCHS}] Итерация [{index}/{len(dataloader)}, Loss: {loss.item()}, Triplets: {mining_func.num_triplets}\]\n"
-            log_to_file(msg)
+            optimizer.step()
 
-    if epoch % 2 == 0:
-        model.eval()
+            if index % 100 == 0:
+                history["train"].append({
+                    "epoch": epoch,
+                    "loss": loss.item(),
+                    "triplets": mining_func.num_triplets
+                })
+                msg = f"Эпоха [{epoch}/{EPOCHS}] Итерация [{index}/{len(dataloader)}, Loss: {loss.item()}, Triplets: {mining_func.num_triplets}\]\n"
+                log_to_file(msg)
 
-        with torch.no_grad():
-            accuracy = get_accuracy(val_dataset, train_dataset, model, DEVICE)
+        if epoch % 2 == 0:
+            model.eval()
 
-            history["val"].append({"epoch": epoch, "accuracy": accuracy})
-            msg = f"Val accuracy: {accuracy}"
-            log_to_file(msg)
+            with torch.no_grad():
+                accuracy = get_accuracy(val_dataset, train_dataset, model, DEVICE)
 
-            torch.save(model.state_dict(), f"{SAVE_PATH}/model_latest.pth")
+                history["val"].append({"epoch": epoch, "accuracy": accuracy})
+                msg = f"Val accuracy: {accuracy}"
+                log_to_file(msg)
 
-            if accuracy >= history["best_accuracy"]:
-                history["best_accuracy"] = accuracy
-                torch.save(model.state_dict(), f"{SAVE_PATH}/model_best.pth")
+                torch.save(model.state_dict(), f"{SAVE_PATH}/model_latest.pth")
 
-            with open(f"{SAVE_PATH}/history.json", "w") as f:
-                f.write(json.dumps(history))
+                if accuracy >= history["best_accuracy"]:
+                    history["best_accuracy"] = accuracy
+                    torch.save(model.state_dict(), f"{SAVE_PATH}/model_best.pth")
+
+                with open(f"{SAVE_PATH}/history.json", "w") as f:
+                    f.write(json.dumps(history))
