b17aa8f079b366698233764172bda0e43da9e52d
root <root@email.com>
2023-05-18 15:22:23 +0300

    FIX args types

--- run_experiment.py
+++ run_experiment.py
@@ -15,16 +15,16 @@ parser.add_argument('--model', default='resnet50',
                     help='Задаёт базовую модель для извлечения признаков. \nДоступные значения: resnet50.')
 parser.add_argument('--loss', default='softmax',
                     help='Задает функцию потерь, которую нужно использовать при обучении. \nДоступные значения: softmax, triplet, arcface.')
-parser.add_argument('--arc_scale', default=30.0, help='Feature_scale для ArcFace.')
-parser.add_argument('--arc_margin', default=0.5, help='Смещение для ArcFace')
+parser.add_argument('--arc_scale', type=float, default=30.0, help='Feature_scale для ArcFace.')
+parser.add_argument('--arc_margin', type=float, default=0.5, help='Смещение для ArcFace')
 parser.add_argument('--optimizer', default='adam',
                     help='Выбор оптимизатора для обучения. \nДоступные значения: adam, sgd')
 parser.add_argument('--scheduler', default='single_step',
                     help='Выбор lr_scheduler\'a, который использовать. \nДоступные значения: single_step, multi_step')
-parser.add_argument('--sc_step_size', default=20, help='Scheduler шаг.')
-parser.add_argument('--batch_size', default=32, help='Размера батча.')
-parser.add_argument('--lr', default=0.0003, help='Задаёт learning rate для оптимизатора.')
-parser.add_argument('--epochs', default=60, help='Количество эпох.')
+parser.add_argument('--sc_step_size', type=int, default=20, help='Scheduler шаг.')
+parser.add_argument('--batch_size', type=int, default=32, help='Размера батча.')
+parser.add_argument('--lr', type=float, default=0.0003, help='Задаёт learning rate для оптимизатора.')
+parser.add_argument('--epochs', type=int, default=60, help='Количество эпох.')
 parser.add_argument('--visualize', action='store_true', default=False, help='Визуализирует изображения результатов ранкинга.')
 parser.add_argument('--log_path', help='Путь сохранения лога обучения.')
 
@@ -35,8 +35,8 @@ datamanager = torchreid.data.ImageDataManager(
     sources='dukemtmcreid',
     height=256,
     width=128,
-    batch_size_train=int(args.batch_size),
-    batch_size_test=int(args.batch_size),
+    batch_size_train=args.batch_size,
+    batch_size_test=args.batch_size,
     combineall=False,
     train_sampler='RandomIdentitySampler' if args.loss == 'triplet' else 'RandomSampler'
 )
@@ -84,7 +84,7 @@ if not os.path.isdir('./logs'):
     os.mkdir('logs')
 
 engine.run(
-    max_epoch=int(args.epochs),
+    max_epoch=args.epochs,
     save_dir=f'logs/resnet50-{args.loss}-{args.epochs}' if not args.log_path else args.log_path,
     print_freq=15
 )
