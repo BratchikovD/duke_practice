407aa02fa4931e6b670e23fff59760d9abfc953a
root <root@email.com>
2023-05-18 11:51:46 +0300

    ADD arcface loss and engine

---
+++ engines/arcface.py
@@ -0,0 +1,37 @@
+from __future__ import division, print_function, absolute_import
+
+from torchreid import engine
+from ..losses.arcface import ArcfaceLoss
+
+
+class ArcFaceEngine(engine.Engine):
+    def __init__(self, datamanager, model, optimizer, feature_scale=30, margin=0.5, scheduler=None):
+        super(ArcFaceEngine, self).__init__(datamanager, True)
+
+        self.model = model
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.register_model('model', model, optimizer, scheduler)
+        self.criterion = ArcfaceLoss(2048, 702, feature_scale, margin)
+
+    def forward_backward(self, data):
+        imgs, pids = self.parse_data_for_train(data)
+
+        imgs = imgs.cuda()
+        pids = pids.cuda()
+
+        outputs, features = self.model(imgs)
+
+        loss = 0
+        loss_summary = {}
+
+        loss = self.compute_loss(self.criterion, features, pids)
+        loss_summary['loss'] = loss
+
+        assert loss_summary
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss_summary
--- losses/arcface.py
+++ losses/arcface.py
@@ -1,6 +1,10 @@
-from __future__ import division, absolute_import
+from __future__ import absolute_import, division
+
+import math
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class ArcfaceLoss(nn.Module):
@@ -8,8 +12,30 @@ class ArcfaceLoss(nn.Module):
 
     Reference: ArcFace: Additive Angular Margin Loss for Deep Face Recognition arXiv:1801.07698
     """
-    def __init__(self):
+    def __init__(self, in_features, out_features, feature_scale=30.0, margin=0.50):
         super(ArcfaceLoss, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.feature_scale = feature_scale
+        self.margin = margin
+        self.cosine_threshold = math.cos(math.pi - margin)
+        self.modified_margin = math.sin(math.pi - margin) * margin
+        self.weight = nn.Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight)
 
-    def forward(self):
-        pass
+    def forward(self, inputs, targets):
+        cosine = F.linear(F.normalize(inputs), F.normalize(self.weight))
+        sin_theta = torch.sqrt((1.0-torch.pow(cosine, 2)).clamp(0, 1))
+
+        phi = cosine*math.cos(self.margin) - sin_theta*math.sin(self.margin)
+        phi = torch.where(cosine > self.cosine_threshold, phi, cosine - self.modified_margin)
+
+        one_hot = torch.zeros(cosine.size(), device='cuda')
+        one_hot.scatter_(1, targets.view(-1, 1).long(), 1)
+
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output *= self.feature_scale
+
+        cross_entropy = nn.CrossEntropyLoss()
+
+        return cross_entropy(output, targets)
