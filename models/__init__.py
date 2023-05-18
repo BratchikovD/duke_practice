e185f2df9bb16e5f0ca5f23189d266c78cdbb58a
root <root@email.com>
2023-05-18 12:42:01 +0300

    UPDATE build_model

--- models/__init__.py
+++ models/__init__.py
@@ -8,10 +8,13 @@ __model_factory = {
 }
 
 
-def build_model(name, num_classes, pretrained=True, use_gpu=True):
+def build_model(name, num_classes, loss='arcface', pretrained=True, use_gpu=True):
     avai_models = list(__model_factory.keys())
     if name not in avai_models:
         raise KeyError
     return __model_factory[name](
-        num_classes=num_classes, pretrained=pretrained, use_gpu=use_gpu
+        num_classes=num_classes,
+        loss=loss,
+        pretrained=pretrained,
+        use_gpu=use_gpu
     )
