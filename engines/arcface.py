060d790fa7b99c14476cafb1aad7b318384a1926
BratchikovD <bratchikov.dmitry@gmail.com>
2023-05-19 23:11:45 +0300

    Experiments settings

--- engines/arcface.py
+++ engines/arcface.py
@@ -5,7 +5,7 @@ from losses import ArcFaceLoss
 
 
 class ImageArcFaceEngine(engine.Engine):
-    def __init__(self, datamanager, model, optimizer, margin=1.5, scheduler=None):
+    def __init__(self, datamanager, model, optimizer, margin=0.8, scheduler=None):
         super(ImageArcFaceEngine, self).__init__(datamanager, True)
 
         self.model = model
