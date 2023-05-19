8fe7c438f8290badb49d8d4e9d7941e0615f9ced
BratchikovD <bratchikov.dmitry@gmail.com>
2023-05-19 22:46:59 +0300

    Experiments settings

--- losses/arcface.py
+++ losses/arcface.py
@@ -18,7 +18,7 @@ class ArcFaceLoss(nn.Module):
         self.in_features = in_features
         self.out_features = out_features
 
-        self.s = 15
+        self.s = 9.2
         self.margin = margin
 
         self.cosine_threshold = math.cos(math.pi - margin)
