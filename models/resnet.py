cb965966d99909ecdd691672566c4148e80c7087
BratchikovD <bratchikov.dmitry@gmail.com>
2023-05-21 15:54:59 +0300

    MOVE arcface layer to model

--- models/resnet.py
+++ models/resnet.py
@@ -18,7 +18,7 @@ class ArcMarginProduct(nn.Module):
         super(ArcMarginProduct, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.s = math.sqrt(2)*math.log(out_features-1)
+        self.s = 30
         self.m = m
         self.weight = nn.Parameter(torch.FloatTensor(out_features, in_features))
         nn.init.xavier_uniform_(self.weight)
