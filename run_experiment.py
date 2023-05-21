a425be3ab244c38a6deb909a5c56ba3a992871de
BratchikovD <bratchikov.dmitry@gmail.com>
2023-05-21 20:15:48 +0300

    REMOVE step on 20epoch

--- run_experiment.py
+++ run_experiment.py
@@ -60,7 +60,7 @@ optimizer = torchreid.optim.build_optimizer(
 scheduler = torchreid.optim.build_lr_scheduler(
     optimizer,
     lr_scheduler='multi_step',
-    stepsize=[5, 15, 25],
+    stepsize=[5, 15],
     max_epoch=args.epochs,
     gamma=args.gamma,
 )
