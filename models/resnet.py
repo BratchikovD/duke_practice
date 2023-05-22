525c660e1cf3d10a45bba9531441149d0e56bde8
BratchikovD <bratchikov.dmitry@gmail.com>
2023-05-22 11:06:55 +0300

    REBUILD sphere implementation

--- models/resnet.py
+++ models/resnet.py
@@ -51,7 +51,7 @@ class ArcMarginProduct(nn.Module):
 class ResnetNewLosses(models.resnet.ResNet):
     def __init__(self, num_classes, loss, block, layers, **kwargs):
         super().__init__(num_classes, loss, block, layers, **kwargs)
-        self.arc_block = ArcMarginProduct(2048, 702, easy_margin=True)
+        self.arc_block = ArcMarginProduct(1024, 702, easy_margin=True)
         self.bn2 = nn.BatchNorm1d(2048)
         self.dp = nn.Dropout(0.5)
         self.fc = nn.Linear(in_features=2048, out_features=1024, bias=True)
